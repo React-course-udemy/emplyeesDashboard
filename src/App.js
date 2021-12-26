aabaea8a1ec7411074b30cba834bb9847fbafe70
Bassant Ahmed <bassant.ayman69@gmail.com>
2021-12-26 11:54:32 +0200

    create todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div>hello!</div>;
-}
+import React from 'react';
+//components
+import Todo from './components/Todo';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="learn Turkish" />
+      <Todo text="learn Greek" />
+      <Todo text="learn React" />
+    </div>
+  );
+};
 
 export default App;
